1cb363a1b2499a8c94408f2e4ef04cec3ba4d064
cetaru <cetarucom@gmail.com>
2022-03-05 18:53:41 -0300

    fix tabulator

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vuetabulator.js', mode: 'client', ssr: false },
+    //{ src: '~/plugins/vuetabulator.js', mode: 'client', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
