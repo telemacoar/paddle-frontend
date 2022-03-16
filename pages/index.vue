b5691edd5d7c57036182ada7f8f0f318fa5e1e6d
cetaru <cetarucom@gmail.com>
2022-03-16 06:42:32 -0300

    fix login form

--- pages/index.vue
+++ pages/index.vue
@@ -30,16 +30,6 @@
         </v-card-actions>
       </v-card>
     </v-col>
-    <v-col>
-      <v-card>
-        <v-card-text>
-          <v-img max-height="150" max-width="250" src="/courts.jpg"></v-img>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn text @click="$router.push('/courts')">Canchas </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
   </v-row>
 </template>
 
