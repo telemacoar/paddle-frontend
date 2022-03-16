29ca922665e0791fb339bfd9628d876faabd17a3
cetaru <cetarucom@gmail.com>
2022-03-15 23:25:02 -0300

    test

--- pages/index.vue
+++ pages/index.vue
@@ -30,6 +30,16 @@
         </v-card-actions>
       </v-card>
     </v-col>
+    <v-col>
+      <v-card>
+        <v-card-text>
+          <v-img max-height="150" max-width="250" src="/courts.jpg"></v-img>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="$router.push('/courts')">Canchas </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
